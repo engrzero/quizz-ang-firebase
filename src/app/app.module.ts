8bb7e27a2f6035a26f066e58eb65239110fc44b1
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-13 16:40:09 -0300

    Inicio Component - Validamos PIN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
@@ -12,7 +15,8 @@ import { InicioComponent } from './components/inicio/inicio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  error = false;
+  pin = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ingresar() {
+
+    // Validar si el usuario no ingreso ningun caracter
+    if(this.pin == '') {
+
+      this.error = true;
+
+      setTimeout(() => {
+        this.error = false;
+      }, 3000);
+    }
   }
 
 }
