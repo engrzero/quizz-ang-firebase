754f98b8f287b8db2b1ffa88666f0b9b3aeec91c
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-14 11:26:18 -0300

    Configuracion de lazy loading y rutas

--- src/app/components/usuario/usuario.module.ts
+++ src/app/components/usuario/usuario.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
+
+// Componentes
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
