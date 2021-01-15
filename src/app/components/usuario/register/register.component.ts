e1493d9671c56028c53ab2ace468cadd1ccd83fe
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-15 09:47:51 -0300

    Register Validaciones

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.registerForm = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      passowrd: ['', [Validators.required, Validators.minLength(6)]],
+      repetirPassword: ['']
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.registerForm);
   }
 
 }
