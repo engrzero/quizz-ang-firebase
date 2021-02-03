279bd22e6746e5ff9f63a6263d4e7c5bc20a89b7
truizdiaz <tomiruizdiaz@gmail.com>
2021-02-03 10:01:02 -0300

    Errores al registrarse

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -13,9 +13,9 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder,
-              private afAuth: AngularFireAuth,
-              private router: Router,
-              private toastr: ToastrService) { 
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private toastr: ToastrService) {
     this.registerForm = this.fb.group({
       usuario: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -29,21 +29,41 @@ export class RegisterComponent implements OnInit {
   register() {
     const usuario = this.registerForm.get('usuario')?.value;
     const password = this.registerForm.get('password')?.value;
-  
-    this.afAuth.createUserWithEmailAndPassword(usuario,password).then(rta => {
+
+    this.afAuth.createUserWithEmailAndPassword(usuario, password).then(rta => {
       this.toastr.success('El usuario fue registrado con exito!', 'Usuario registrado!');
       this.router.navigate(['/usuario'])
     }).catch(error => {
-      console.log(error);
-      this.toastr.error('Error','Opss ocurrio un error');
+      this.toastr.error(this.error(error.code), 'Error');
     })
 
+  }
+
+  error(code: string): string {
+
+    switch (code) {
+
+      // Email ya registrado
+      case 'auth/email-already-in-use':
+        return 'El Correo ya esta registrado'
+
+      // Correo invalido
+      case 'auth/invalid-email':
+        return 'El Correo es invalido'
+
+      // La Contraseña es muy debil
+      case 'auth/weak-password':
+        return 'La Contraseña es muy debil'
+
+      default:
+        return 'Error desconocido';
+    }
   }
 
   checkPassword(group: FormGroup): any {
     const pass = group.controls.password?.value;
     const confirmPassword = group.controls.repetirPassword?.value;
-    return pass === confirmPassword ? null :  { notSame: true }
+    return pass === confirmPassword ? null : { notSame: true }
   }
 
 }
