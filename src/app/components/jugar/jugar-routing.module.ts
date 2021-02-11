080a1a9da6b024d0b69b64e332f80e40333732d4
truizdiaz <tomiruizdiaz@gmail.com>
2021-02-11 11:41:06 -0300

    Contador Inicial

--- src/app/components/jugar/jugar-routing.module.ts
+++ src/app/components/jugar/jugar-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 // Componentes
 import { ContadorInicialComponent } from './contador-inicial/contador-inicial.component';
 import { IngresarNombreComponent } from './ingresar-nombre/ingresar-nombre.component';
+import { RealizarQuizzComponent } from './realizar-quizz/realizar-quizz.component';
 
 const routes: Routes = [
   { path: '', component: IngresarNombreComponent },
-  { path: 'iniciarContador', component: ContadorInicialComponent }
+  { path: 'iniciarContador', component: ContadorInicialComponent },
+  { path: 'realizarQuizz', component: RealizarQuizzComponent },
 ];
 
 @NgModule({
